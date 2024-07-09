1ee650f0c761664e61a460d2be4bcd1b27c72d68
andrew <baby-shit@yandex.ru>
2024-07-09 23:52:20 +0300

    many fixes

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 import asyncio
-from aiogram import types, Dispatcher
 from config.log_config import setup_logging
 from utils import csv_utils
 from config.config import API_TOKEN
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, types
 from handlers.start import router
 from handlers.add_employee import router_add_employee
 from handlers.remove_employee import router_remove_employee
-
 
 bot = Bot(token=API_TOKEN)
 
@@ -21,16 +19,23 @@ async def set_default_commands(bot):
     await bot.set_my_commands(commands)
 
 
+async def check_anniversary_periodically():
+    while True:
+        await csv_utils.check_anniversary()
+        await asyncio.sleep(12 * 60 * 60)  # 12 часов в секундах
+
+
+
 async def main():
     setup_logging()
     dp = Dispatcher()
     dp.include_routers(router, router_add_employee, router_remove_employee)
     await set_default_commands(bot)
-    await csv_utils.check_anniversary()
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
+    loop.create_task(check_anniversary_periodically())
     loop.create_task(main())
     loop.run_forever()
