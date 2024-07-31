2e3893526715a4cbf65af46802f9f16cc6da0d11
andrew <baby-shit@yandex.ru>
2024-07-31 21:44:18 +0300

    update trigger

--- config/scheduler.py
+++ config/scheduler.py
@@ -14,7 +14,7 @@ def also_bot_scheduler() -> AsyncIOScheduler:
     scheduler = AsyncIOScheduler()
     scheduler.add_listener(job_execution_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
     scheduler.add_job(check_anniversary, 'cron', hour=10, minute=0)
-    scheduler.add_job(send_quote, 'cron', hour=20, minute=0)
+    scheduler.add_job(send_quote, 'cron', hour=9, minute=30)
     return scheduler
 
 
